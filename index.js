77d301760396608dc361c390f4aacf488e7e9565
apulkGIT <138578834+apulkGIT@users.noreply.github.com>
2025-01-01 14:56:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+//app.use(express.static(path.join(__dirname, "files")));
+
+app.get("/",(req,res) => {
+  //res.sendFile(path.join(__dirname,'.',"index.html")); 
+  res.send("<H1>Welcome again!What up?</H1>");
+});
+
 console.log("workking");
